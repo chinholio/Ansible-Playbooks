33982035f7b128f2572fe7b179cf5880279c10d3
chinholio <37453444+chinholio@users.noreply.github.com>
2024-03-19 16:23:55 +0000

    Update py_ftp_check.py
    Updating to nlst

--- Common/py_ftp_check.py
+++ Common/py_ftp_check.py
@@ -2,7 +2,7 @@ from ftplib import FTP
 import re
 from datetime import datetime, timedelta
 
-def get_most_recent_file_ftp(host, username, password, directory):
+def get_most_recent_file_ftp_nlst(host, username, password, directory):
     try:
         # Connect to FTP server
         ftp = FTP(host)
@@ -16,18 +16,23 @@ def get_most_recent_file_ftp(host, username, password, directory):
             new_directory = re.sub(r'\d{4}-\d{2}-\d{2}', 'backup', directory)
             ftp.cwd(new_directory)
 
-        # List directory contents using MLSD
-        files = ftp.mlsd()
+        # List directory contents using NLST
+        files = ftp.nlst()
 
         # Get the most recent file
         most_recent_file = None
         most_recent_time = datetime.min
 
-        for file, metadata in files:
-            file_time = datetime.strptime(metadata['modify'], '%Y%m%d%H%M%S')
-            if file_time > most_recent_time:
-                most_recent_time = file_time
-                most_recent_file = file
+        for file in files:
+            try:
+                file_time = ftp.sendcmd(f"MDTM {file}")
+                file_time = datetime.strptime(file_time[4:], '%Y%m%d%H%M%S')
+                if file_time > most_recent_time:
+                    most_recent_time = file_time
+                    most_recent_file = file
+            except Exception:
+                # Ignore files that don't support MDTM command
+                pass
 
         # Check if the most recent file was created within the last 1 day
         current_time = datetime.now()
@@ -47,5 +52,5 @@ username = "your_username"
 password = "your_password"
 directory = "/path/to/ftp/directory"
 
-result = get_most_recent_file_ftp(host, username, password, directory)
+result = get_most_recent_file_ftp_nlst(host, username, password, directory)
 print(result)
